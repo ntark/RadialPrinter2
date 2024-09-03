d7fc9bb394f1e7ac7521f9a86387360980f4a1c5
ntark <ntark20@freeuni.edu.ge>
2024-09-03 23:11:56 +0400

    allow multiple steps per call

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -35,6 +35,27 @@ namespace RadialPrinter.Controllers
             }
         }
 
+        [HttpPost("toGCode")]
+        public async Task<IActionResult> ToGCode(IFormFile file)
+        {
+            try
+            {
+                var filePath = await FileHelper.UploadFile(file);
+
+                var svgPath = await PythonAPIHelper.ImageToSvg(filePath);
+
+                var resPath = await PythonAPIHelper.SvgToGCode(svgPath);
+
+                var fileStream = new FileStream(resPath, FileMode.Open, FileAccess.Read);
+
+                return File(fileStream, "application/octet-stream", Path.GetFileName(resPath));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
         [HttpPost("toEdges")]
         public async Task<IActionResult> ToEdges(IFormFile file)
         {
@@ -53,5 +74,50 @@ namespace RadialPrinter.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPost("toEdgesSvg")]
+        public async Task<IActionResult> ToEdgesSvg(IFormFile file)
+        {
+            try
+            {
+                var filePath = await FileHelper.UploadFile(file);
+
+                var edgesPath = await PythonAPIHelper.ImageToEdges(filePath);
+
+                var resPath = await PythonAPIHelper.ImageToSvg(edgesPath);
+
+                var fileStream = new FileStream(resPath, FileMode.Open, FileAccess.Read);
+
+                return File(fileStream, "application/octet-stream", Path.GetFileName(resPath));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("toEdgesGCode")]
+        public async Task<IActionResult> ToEdgesGCode(IFormFile file)
+        {
+            try
+            {
+                var filePath = await FileHelper.UploadFile(file);
+
+                var edgesPath = await PythonAPIHelper.ImageToEdges(filePath);
+
+                var svgPath = await PythonAPIHelper.ImageToSvg(edgesPath);
+
+                var resPath = await PythonAPIHelper.SvgToGCode(svgPath);
+
+                var fileStream = new FileStream(resPath, FileMode.Open, FileAccess.Read);
+
+                return File(fileStream, "application/octet-stream", Path.GetFileName(resPath));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
