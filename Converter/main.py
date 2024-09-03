5ec50bbefbf006e68527297e42f360021fd04f38
ntark <ntark20@freeuni.edu.ge>
2024-09-03 19:20:23 +0000

    clean python file

--- Converter/main.py
+++ Converter/main.py
@@ -47,39 +47,6 @@ def svg_to_gcode(in_path, out_path):
     os.chdir(script_path)
 
 
-# def process_file():
-#     img_path = sys.argv[1]
-#     random_string = sys.argv[2]
-
-#     img = cv2.imread(img_path)
-
-#     # random_number = random.randint(10000000, 16777215)
-#     # random_string = str(hex(random_number))
-#     # random_string = f"{random_string[2:]}"
-
-#     image_bmp_path = f"image_{random_string}.bmp"
-#     image_svg_path = f"image_{random_string}.svg"
-#     image_gcode_path = f"image_{random_string}.gcode"
-#     edges_bmp_path = f"edges_{random_string}.bmp"
-#     edges_svg_path = f"edges_{random_string}.svg"
-#     edges_gcode_path = f"edges_{random_string}.gcode"
-
-#     temp_path = f"/home/opc/workspace/works/c#/RadialPrinter2/Converter/temp"
-#     cargo_path = f"/home/opc/workspace/works/c#/RadialPrinter2/Converter/svg2gcode"
-
-#     cv2.imwrite(os.path.join(temp_path, image_bmp_path), img)
-
-#     image_to_svg(temp_path, image_bmp_path, image_svg_path)
-#     svg_to_gcode(temp_path, cargo_path, image_svg_path, image_gcode_path)
-
-#     edges = canny_edge_detection(img)
-
-#     cv2.imwrite(os.path.join(temp_path, edges_bmp_path), edges)
-
-#     image_to_svg(temp_path, edges_bmp_path, edges_svg_path)
-#     svg_to_gcode(temp_path, cargo_path, edges_svg_path, edges_gcode_path)
-
-
 def image_to_bmp(in_path, out_path):
     img = cv2.imread(in_path)
     cv2.imwrite(out_path, img)
@@ -87,11 +54,6 @@ def image_to_bmp(in_path, out_path):
 
 app = Flask(__name__)
 
-# image to svg
-# svg to gcode
-# image to edges
-# -- edges to svg edges
-# -- svg edges to svg gcode
 
 TEMP_FLODER_PATH = "/home/opc/workspace/works/csharp/RadialPrinter2/Converter/temp/"
 CARGO_FOLDER_PATH = (
